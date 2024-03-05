0632d2aa5d7c2ca01dc115de3698184849699d08
thomas3456787654 <thomas.hendrickson@s.capregboces.org>
2024-03-05 09:19:29 -0500

    complete

---
+++ Execise8/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Execise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter maximum number: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                //for loop body
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            //print asteisks
+            
+
+            //Console.WriteLine(number);
+        }
+    }
+}
